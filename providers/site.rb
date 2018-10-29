ea6c39b671433d89dd6c78e1aba46a92663c9264
Michael F. Weinberg <michael@luckymike.com>
2018-10-29 12:46:10 -0700

    Remove pleaserun usage.

--- providers/site.rb
+++ providers/site.rb
@@ -61,12 +61,10 @@ action :create do
     notifies :restart, "service[oauth2_proxy-#{new_resource.name}]"
   end
 
-  pleaserun "oauth2_proxy-#{new_resource.name}" do
-    name "oauth2_proxy-#{new_resource.name}"
-    program ::File.join(node['oauth2_proxy']['install_path'],'oauth2_proxy')
-    args [ "-config=#{oauth2_proxy_cfg_path}" ]
-    description "oauth2_proxy for #{new_resource.name}"
-    action :create
+  template "/etc/systemd/system/oauth2_proxy-#{new_resource.name}.service" do
+    cookbook 'oauth2_proxy'
+    source 'systemd.conf.erb'
+    variables name: new_resource.name
   end
 
   service "oauth2_proxy-#{new_resource.name}" do
